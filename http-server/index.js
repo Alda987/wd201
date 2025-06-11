94d9da0a94a72f15bc495efda24493793dbc72e8
Aldatreesa JOSY <aldatreesajosy05@gmail.com>
2025-06-11 21:28:32 +0530

    Fix error and complete registration functionality

--- http-server/index.js
+++ http-server/index.js
@@ -1,35 +1,51 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const minimist = require('minimist');
 
-// Parse command line argument
 const args = minimist(process.argv.slice(2));
 const port = args.port || 3000;
 
 const server = http.createServer((req, res) => {
-  let filePath = '';
-  
-  if (req.url === '/' || req.url === '/home') {
-    filePath = path.join(__dirname, 'home.html');
-  } else if (req.url === '/projects') {
-    filePath = path.join(__dirname, 'project.html');
-  } else if (req.url === '/registration') {
-    filePath = path.join(__dirname, 'registration.html');
+  const parsedUrl = url.parse(req.url, true); // parse query params
+  const pathname = parsedUrl.pathname;
+
+  if (pathname === '/' || pathname === '/home') {
+    sendFile(res, 'home.html');
+  } else if (pathname === '/projects') {
+    sendFile(res, 'project.html');
+  } else if (pathname === '/registration' && Object.keys(parsedUrl.query).length === 0) {
+    // No query => Show the form
+    sendFile(res, 'registration.html');
+  } else if (pathname === '/registration' && Object.keys(parsedUrl.query).length > 0) {
+    // Form submitted => Show submitted data
+    const { name, email, password, dob, terms } = parsedUrl.query;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`
+      <h1>Registration Successful</h1>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Date of Birth:</strong> ${dob}</p>
+      <p><strong>Accepted Terms:</strong> ${terms ? 'Yes' : 'No'}</p>
+    `);
   } else {
     res.writeHead(404);
-    return res.end('Page not found');
+    res.end('Page not found');
   }
+});
 
+function sendFile(res, fileName) {
+  const filePath = path.join(__dirname, fileName);
   fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(500);
-      return res.end(`Error loading ${filePath}`);
+      return res.end(`Error loading ${fileName}`);
     }
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   });
-});
+}
 
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
