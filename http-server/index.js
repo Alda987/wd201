e93279c9332e2719f495b588f2bbfc42c2878289
Aldatreesa JOSY <aldatreesajosy05@gmail.com>
2025-06-13 05:41:35 +0530

    Add http-server project

---
+++ http-server/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = "";
+
+  if (req.url === "/" || req.url === "/home.html") {
+    filePath = path.join(__dirname, "home.html");
+  } else if (req.url === "/project.html") {
+    filePath = path.join(__dirname, "project.html");
+  } else if (req.url === "/registration.html") {
+    filePath = path.join(__dirname, "registration.html");
+  } else if (req.url === "/index.js") {
+    filePath = path.join(__dirname, "index.js");
+    res.setHeader("Content-Type", "text/javascript");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+    return;
+  }
+
+  // Set default content type
+  const ext = path.extname(filePath);
+  const contentType = {
+    ".html": "text/html",
+    ".js": "text/javascript"
+  }[ext] || "text/plain";
+
+  res.writeHead(200, { "Content-Type": contentType });
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Internal Server Error");
+    } else {
+      res.end(content);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
